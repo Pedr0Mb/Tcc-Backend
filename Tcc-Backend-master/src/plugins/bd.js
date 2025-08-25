baf3d0002332789144126bb5cc316616b17b3ad9
Pedr0Mb <160172174+Pedr0Mb@users.noreply.github.com>
2025-08-25 16:17:18 -0300

    Delete Tcc-Backend-master directory

--- Tcc-Backend-master/src/plugins/bd.js
+++
@@ -1,8 +0,0 @@
-import mysql from 'mysql2/promise';
-
-export const db = await mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '8014154080Mp',
-  database: 'participacaoCidadaBd'
-});
--- Tcc-Backend-master/src/routers/voto.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as votoController from '../controllers/votoController.js'
-import { autenticarToken } from '../middlewares/autenticarToken.js';
-
-const router = express.Router();
-
-router.post('/', autenticarToken, votoController.RegistrarVoto)
-
-export default router;
